192f2c567896dea8a820cf6968af5ba7cf57dc72
CrewMoon <kemu.xu20@student.xjtlu.edu.cn>
2024-05-03 23:06:01 +0800

    finished AIChessPlayer.cs, some realization also made on Board.cs but should be checked by Zhen Ma.

--- A2/Assets/Scripts/Board/Board.cs
+++ A2/Assets/Scripts/Board/Board.cs
@@ -61,10 +61,82 @@ public class Board
         // A player2's pawn is on this position.
         PLAYER2
     }
+    
+    /// <summary>
+    /// check if the move is a clone move
+    /// </summary>
+    /// <param name="move"></param>
+    /// <returns>
+    /// return true when:
+    ///     1. src and des are not null
+    ///     2. src and des are both in board
+    ///     3. src and des are not in the same pos
+    ///     5. Manhattan distance between src and dst in the range of 1</returns>
+    public static bool isClone(ChessMove move)
+    {
+        Vector2Int src = move.getSrc();
+        Vector2Int dst = move.getDst();
+        
+        if(src == null || dst == null)
+        {
+            return false;
+        }
+
+        if(isPosInBoard(src) || !isPosInBoard(dst))
+        {
+            return false;
+        }
+
+        if(src.Equals(dst))
+        {
+            return false;
+        }
+        
+        return Math.Abs(src.x - dst.x) <= 1 && Math.Abs(src.y - dst.y) <= 1;
+    }
+    
+    /// <summary>
+    /// check if the move is a jump move
+    /// </summary>
+    /// <param name="move"></param>
+    /// <returns>
+    /// return true when:
+    ///     1. src and des are not null
+    ///     2. src and des are both in board
+    ///     3. src and des are not in the same pos
+    ///     4. not clone move
+    ///     5. Manhattan distance between src and dst in the range of 2</returns>
+    public static bool isJump(ChessMove move)
+    {
+        Vector2Int src = move.getSrc();
+        Vector2Int dst = move.getDst();
+        
+        if(src == null || dst == null)
+        {
+            return false;
+        }
+
+        if(isPosInBoard(src) || !isPosInBoard(dst))
+        {
+            return false;
+        }
+
+        if(src.Equals(dst))
+        {
+            return false;
+        }
+
+        if (!isClone(move))
+        {
+            return false;
+        }
+        return Math.Abs(src.x - dst.x) <= 2 && Math.Abs(src.y - dst.y) <= 2;
+    }
 
     /********************************** FIELDS ************************************/
 
     // TODO: Defined By Zhen Ma
+    private BoardPositionState[,] board;
 
     /********************************** CTOR ************************************/
 
@@ -86,6 +158,29 @@ public class Board
         UnityEngine.Vector2Int player2StartPos
     )
     {
+        // initialise the board
+        this.board = new BoardPositionState[BOARD_LENGTH, BOARD_LENGTH];
+        for (int i = 0; i < BOARD_LENGTH; i++)
+        {
+            for (int j = 0; j < BOARD_LENGTH; j++)
+            {
+                if (i == player1StartPos.x && j == player1StartPos.y)
+                {
+                    this.board[i, j] = BoardPositionState.PLAYER1;
+                }
+                else if(i == player2StartPos.x && j == player2StartPos.y)
+                {
+                    this.board[i, j] = BoardPositionState.PLAYER2;
+                }
+                else
+                {
+                    this.board[i, j] = BoardPositionState.EMPTY;
+                }
+            }
+        }
+        
+        // TODO: Defined By Zhen Ma (I have only initialised the board cuz I need it. Other functions have not implemented)
+        
         throw new System.NotImplementedException();
     }
 
@@ -103,10 +198,41 @@ public class Board
     /// <summary>
     /// Calculates the state of the board.
     /// </summary>
-    /// <returns>the current state</returns>
+    /// <returns>the current state
+    /// In my(kemu) realization, the state is: (--> need to be checked by Zhen Ma)
+    /// BoardState.PLAYER2_WON if player2 has won (1. player1 has no pawns left; OR 2. both players cannot move and player2 has more pawns);
+    /// BoardState.PLAYER1_WON if player1 has won (1. player2 has no pawns left; OR 2. both players cannot move and player1 has more pawns)
+    /// BoardState.DRAW if the game is draw (both player cannot move and have the same number of pawns).
+    /// BoardState.PLAYING if the game is not end (no situation above happens);
+    /// </returns>
     public BoardState getBoardState()
     {
-        throw new System.NotImplementedException();
+        Vector3Int result = getBoardStatistics();
+        if(result.y == 0)
+        {
+            return BoardState.PLAYER2_WON;
+        }else if (result.z == 0)
+        {
+            return BoardState.PLAYER1_WON;
+        }else if(!couldMove(BoardPositionState.PLAYER1) && !couldMove(BoardPositionState.PLAYER2))
+        {
+            if (result.y > result.z)
+            {
+                return BoardState.PLAYER1_WON;
+            }
+            else if (result.y < result.z)
+            {
+                return BoardState.PLAYER2_WON;
+            }
+            else
+            {
+                return BoardState.DRAW;
+            }
+        }
+        else
+        {
+            return BoardState.PLAYING;
+        }
     }
 
     /// <param name="position"></param>
@@ -158,13 +284,112 @@ public class Board
         throw new System.NotImplementedException();
     }
 
+    /// <summary>
+    /// In this function, we ensure this move is legal in the board
+    /// a move is legal when the following requirements are met:
+    ///     1. the player is not empty
+    ///     2. Manhattan distance between src and dst in the range of 2
+    ///         -- move is forfeit
+    ///             1. one player could move (--> should be discussed somehow, I am not sure)
+    ///         -- move is not forfeit
+    ///             1. des is in the range of board and not empty
+    ///             2. current player is at src
+    /// </summary>
+    /// <param name="move"></param>
+    /// <returns></returns>
+    public bool checkMove(ChessMove move)
+    {
+        // Determine the current player
+        BoardPositionState currentPlayer = move.getPlayer();
+        
+        // check if the player is empty
+        if (currentPlayer == BoardPositionState.EMPTY)
+        {
+            return false;
+        }
+        
+        // check if the dst is in the range of 5*5 box taking the src as the center
+        Vector2Int src = move.getSrc();
+        Vector2Int dst = move.getDst();
+        if (!(Math.Abs(src.x - dst.x) <= 2 && Math.Abs(src.y - dst.y) <= 2))
+        {
+            return false;
+        }
+
+        // Determine the next player
+        BoardPositionState nextPlayer = (currentPlayer == BoardPositionState.PLAYER1 ? 
+            BoardPositionState.PLAYER2 : BoardPositionState.PLAYER1);
+            
+        // Check if the move is forfeited
+        if (move.isForfeited())
+        {
+            return (couldMove(currentPlayer) || couldMove(nextPlayer));
+        }
+        else
+        {
+            if(!isPosInBoard(move.getDst())
+               || getBoardPosState(move.getDst()) != BoardPositionState.EMPTY)
+            {
+                return false;
+            }
+            else
+            {
+                if (getBoardPosState(move.getSrc()) != currentPlayer)
+                {
+                    return false;
+                }
+
+                return true;
+            }
+        }
+    }
+    
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="who"></param>
+    /// <returns> Return true iff player WHO could move, ignoring whether it is
+    /// that player's move and whether the getAtaxxGame is over.</returns>
+    private bool couldMove(BoardPositionState who)
+    {
+        for (int i = 0; i < BOARD_LENGTH; i++)
+        {
+            for (int j = 0; j < BOARD_LENGTH; j++)
+            {
+                if (getBoardPosState(new Vector2Int(i, j)) == who)
+                {
+                    for (int m = -2; m <= 2; m++)
+                    {
+                        for (int n = -2; n <= 2; n++)
+                        {
+                            if (m !=0 && n != 0)
+                            {
+                                Vector2Int temp = new Vector2Int(i + m, j + n);
+                                if(temp.x >= 0 
+                                   && temp.x < BOARD_LENGTH 
+                                   && temp.y >= 0 
+                                   && temp.y < BOARD_LENGTH
+                                   && getBoardPosState(temp) == BoardPositionState.EMPTY)
+                                {
+                                    return true;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
     /********************************** MUTATORS ************************************/
 
+    // REMINDER: the statement enclosed by "()" is added by kemu
     /// <summary>
     /// Make a move on the board.
     /// </summary>
     /// <returns>
-    /// A list of all posistions where the opponent's pawns
+    /// A list of all positions where the opponent's pawns
     /// have been turned into the pawns of the player who made the move.
     /// 
     /// The game may use the list to know which GameObjects to create/destroy,
@@ -173,13 +398,52 @@ public class Board
     /// 
     /// <exception cref="System.ArgumentException">
     /// 1. if the move is illegal or not possible.
+    /// (2. if the game is end.)
     /// </exception>
     public List<UnityEngine.Vector2Int> makeMove
     (
         ChessMove move
     )
     {
-        throw new System.NotImplementedException();
+        // throw an exception if the move is illegal or the game is end
+        if(!checkMove(move) 
+           || !isClone(move)
+           || !isJump(move)
+           || getBoardState() != BoardState.PLAYING)
+        {
+            throw new System.ArgumentException("The move is illegal or the game is end.");
+        }
+
+        // return an empty list if the move is forfeited
+        if (move.isForfeited())
+        {
+            return new List<Vector2Int>();
+        }
+
+        List<UnityEngine.Vector2Int> result = new List<Vector2Int>();
+        BoardPositionState opponent = (move.getPlayer() == BoardPositionState.PLAYER1 ? 
+            BoardPositionState.PLAYER2 : BoardPositionState.PLAYER1);
+
+        // if the move is a jump move, remove the src
+        if (isJump(move))
+        {
+            board[move.getSrc().x, move.getSrc().y] = BoardPositionState.EMPTY;
+        }
+        
+        // convert dst to player's pawn, and all opponent's pawns within the Manhattan distance of 1 to player's pawn
+        board[move.getDst().x, move.getDst().y] = move.getPlayer();
+        for (int i = move.getDst().x - 1; i <= move.getDst().x + 1; i++)
+        {
+            for (int j = move.getDst().y - 1; j <= move.getDst().y + 1; j++)
+            {
+                if (getBoardPosState(new Vector2Int(i, j)) == opponent)
+                {
+                    board[i, j] = move.getPlayer();
+                    result.Add(new Vector2Int(i, j));
+                }
+            }
+        }
+        return result;
     }
 
     /// <summary>
--- A2/Assets/Scripts/Player/AIChessPlayer.cs
+++ A2/Assets/Scripts/Player/AIChessPlayer.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections;
 using System.Threading;
+using UnityEngine;
 
 /// <summary>
 /// Represents an AI chess player.
@@ -11,9 +13,24 @@ public class AIChessPlayer : ConcreteChessPlayer
     // Used to terminate the aiThread
     private IntPtr aiThreadHandle = default(IntPtr);
     private Board chessBoardRef;
+    
+    
+    // determine the AI difficulty, namely the step depth AI will contemplate
+    private int AIDifficulty;
+    
+    // the final chess move ai will take
+    private ChessMove lastFoundMove;
 
     // TODO: decided by Kemu Xu
     private readonly uint difficulty;
+    private readonly ChessMove passMove;
+
+    private enum AIDifficultyState
+    {
+        EASY = 1,
+        NORMAL = 2,
+        HARD = 3
+    }
 
     /********************************** CTOR ************************************/
     public AIChessPlayer(Board.BoardPositionState side, uint difficulty) : base(side)
@@ -72,6 +89,199 @@ public class AIChessPlayer : ConcreteChessPlayer
     /// <returns>the move made</returns>
     private ChessMove aiTask()
     {
+        AIDifficulty = (int)(AIDifficultyState)difficulty;
+        ChessMove aiMove = findMove();
+        
         throw new System.NotImplementedException();
     }
+
+    /// <summary>
+    /// find the best move for the AI player by the miniMax algorithm
+    /// </summary>
+    /// <returns>the best move</returns>
+    private ChessMove findMove()
+    {
+        Board contemplateBoard = new Board(chessBoardRef);
+        // default chess move to help check the existence of valid move,
+        // default chess move is to stay the last move to the destination position
+        lastFoundMove = new ChessMove(getSide(), getMove().getDst(), getMove().getDst());
+        
+        // Minimax Search
+        miniMax(contemplateBoard, AIDifficulty, true, 1, int.MinValue, int.MaxValue);
+
+        return lastFoundMove;
+    }
+
+    /// <summary>
+    /// Use Minimax algorithm with alpha-beta pruning
+    /// to RECURSIVELY find a move from position BOARD and return its
+    /// alpha or beta value, recording the move found in lastFoundMove iff saveMove.
+    /// For Max Operation, if SENSE == 1,
+    /// The move should have maximal value or have value >= BETA -> pruning,
+    /// For Min Operation, if SENSE == -1,
+    /// The move should have minimal value or have value <= ALPHA -> pruning.
+    /// Searches up to DEPTH levels. Searching at level 0 simply returns a static estimate
+    /// of the board value and does not set lastFoundMove. If the game is over
+    /// on BOARD, does not set lastFoundMove either.
+    /// </summary>
+    /// <param name="board">current board to find a move.</param>
+    /// <param name="depth">denotes search depth from MAX_DEPTH to 0, minus 1 reach call.</param>
+    /// <param name="saveMove">indicates only the first recursive call should save the found move.</param>
+    /// <param name="sense">uses 1 and -1 to indicate Min (-1) or Max (1) operations.</param>
+    /// <param name="alpha">denotes the lower bound of the best known choice when searching to the current node.</param>
+    /// <param name="beta">denotes the upper bound of the worst ending when searching down from this node.</param>
+    /// <returns>the alpha or beta value depending on min or max operation.</returns>
+    private int miniMax(Board board, int depth, bool saveMove, int sense, int alpha, int beta)
+    {
+        // initialize the return value
+        int alphaOrBeta = 0;
+        // get the list of all possible ChessMove including forfeit move
+        ArrayList listOfMoves = possibleMoves(board, getSide());
+        bool passLegal;
+        try
+        {
+            passLegal = board.checkMove(passMove);
+        }
+        catch (Exception e)
+        {
+            passLegal = false;
+        }
+
+        if (passLegal)
+        {
+            listOfMoves.Add(passMove);
+        }
+
+        // make the sequence of the move list random
+        listOfMoves = new ArrayList(makeSequenceRandom(listOfMoves));
+
+        // max operation
+        if (sense == 1)
+        {
+            foreach (ChessMove move in listOfMoves)
+            {
+                Board copy = new Board(board);
+                copy.makeMove(move);
+                // get the worst scores selected from its child moves
+                int response = miniMax(copy, depth - 1, false, -1, alpha, beta);
+                // select the best score from the worst scores
+                if (response > alpha)
+                {
+                    alpha = response;
+                    if (saveMove)
+                    {
+                        lastFoundMove = move;
+                    }
+                }
+                // pruning
+                if (alpha >= beta)
+                {
+                    break;
+                }
+            }
+
+            alphaOrBeta = alpha;
+        }
+        // min operation
+        else if (sense == -1)
+        {
+            foreach (ChessMove move in listOfMoves)
+            {
+                Board copy = new Board(board);
+                copy.makeMove(move);
+                // get the best scores selected from its child moves
+                int response = miniMax(copy, depth - 1, false, 1, alpha, beta);
+                // select the worst score from the best scores
+                if (response < beta)
+                {
+                    beta = response;
+                }
+                // pruning
+                if(alpha >= beta)
+                {
+                    break;
+                }
+            }
+
+            alphaOrBeta = beta;
+        }
+
+        return alphaOrBeta;
+    }
+
+    /// <summary>
+    /// make the sequence of the move list in a random way instead of the search way
+    /// </summary>
+    /// <param name="listOfMoves"></param>
+    /// <returns>the list of moves in a random sequence</returns>
+    private ArrayList makeSequenceRandom(ArrayList listOfMoves)
+    {
+        for (int i = 0; i < listOfMoves.Count; i++)
+        {
+            int randomIndex = UnityEngine.Random.Range(i, listOfMoves.Count);
+            ChessMove temp = (ChessMove)listOfMoves[i];
+            listOfMoves[i] = listOfMoves[randomIndex];
+            listOfMoves[randomIndex] = temp;
+        }
+
+        return listOfMoves;
+    }
+
+    /// <summary>
+    /// get all possible moves for the player
+    /// In this function, all positions owned by player are iterated for possible moves.
+    /// And all possible moves for each position are checked by the assistPossibleMoves function.
+    /// </summary>
+    /// <param name="board"> the current board</param>
+    /// <param name="player">the player who will make the chess move</param>
+    /// <returns> all possible moves for the player</returns>
+    private ArrayList possibleMoves(Board board, Board.BoardPositionState player)
+    {
+        ArrayList possibleMoves = new ArrayList();
+
+        for (int i = 0; i < Board.BOARD_LENGTH; i++)
+        {
+            for (int j = 0; j < Board.BOARD_LENGTH; j++)
+            {
+                Vector2Int pos = new Vector2Int(i, j);
+                if (board.getBoardPosState(pos) == player)
+                {
+                    ArrayList addMoves = assistPossibleMoves(board, player, pos);
+                    possibleMoves.AddRange(addMoves);
+                }
+            }
+        }
+
+        return possibleMoves;
+    }
+
+    /// <summary>
+    /// get all legal moves in the board for the player at the position 'pos' to make
+    /// </summary>
+    /// <param name="board">the current board</param>
+    /// <param name="player">the player who will make the chess move</param>
+    /// <param name="pos">the original position where the player will move from</param>
+    /// <returns>all legal moves in the board for the player at the position 'pos' to make</returns>
+    private ArrayList assistPossibleMoves(Board board, Board.BoardPositionState player, Vector2Int pos)
+    {
+        ArrayList assistPossibleMoves = new ArrayList();
+        for (int i = -2; i <= 2; i++)
+        {
+            for (int j = -2; j <= 2; j++)
+            {
+                if(i != 0 && j != 0)
+                {
+                    Vector2Int possibleDst = new Vector2Int(pos.x + i, pos.y + j);
+                    ChessMove possibleMove = new ChessMove(player, pos, possibleDst);
+                    if (board.checkMove(possibleMove))
+                    {
+                        assistPossibleMoves.Add(possibleMove);
+                    }
+                }
+            }
+        }
+
+        return assistPossibleMoves;
+    }
+
 }
